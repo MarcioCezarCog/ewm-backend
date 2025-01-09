34b8926be50ebb80c81cc2f3d9da649cc580e687
root <root@LTBR100439.cts.com>
2025-01-09 02:35:19 -0300

    chnage code

--- src/main/java/com/embraer/ewmbanckend/EwmBanckendApplication.java
+++ src/main/java/com/embraer/ewmbanckend/EwmBanckendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EwmBanckend2Application {
+public class EwmBanckendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EwmBanckend2Application.class, args);
+        SpringApplication.run(EwmBanckendApplication.class, args);
     }
 
 }
