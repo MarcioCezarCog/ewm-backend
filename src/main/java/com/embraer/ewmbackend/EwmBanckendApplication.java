d285fd94aa4fe1dc5f04ea01528ea1702e3dbccf
root <root@LTBR100439.cts.com>
2025-01-09 04:44:24 -0300

    change code

--- src/main/java/com/embraer/ewmbackend/EwmBanckendApplication.java
+++ src/main/java/com/embraer/ewmbackend/EwmBanckendApplication.java
@@ -1,4 +1,4 @@
-package com.embraer.ewmbanckend;
+package com.embraer.ewmbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
