3ef9fa5be8a9dd3f344621ff5c58d25be26f2863
Marcio Cezar <31347473+marciorcezar@users.noreply.github.com>
2025-01-09 12:01:30 -0300

    all code

---
+++ src/main/java/com/embraer/ewmbanckend/EwmBanckend2Application.java
@@ -0,0 +1,13 @@
+package com.embraer.ewmbanckend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwmBanckend2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EwmBanckend2Application.class, args);
+    }
+
+}
